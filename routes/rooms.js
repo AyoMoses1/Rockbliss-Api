4969b1b112cb864fdd8ee89647f761124b28da84
Ayo Moses <ayomoses@afrimash.com>
2022-10-11 10:36:01 +0100

    pushed api to remote server

---
+++ routes/rooms.js
@@ -0,0 +1,25 @@
+import express from "express"
+import { createRoom, updateRoom, deleteRoom, getRoom, getRooms, updateRoomAvailability } from "../controllers/room.js";
+import { verifyAdmin } from "../utils/verifyToken.js"
+
+const router = express.Router();
+
+
+//CREATE
+router.post("/:hotelId", verifyAdmin, createRoom);
+
+//UPDATE
+router.put("/:id", verifyAdmin, updateRoom);
+
+router.put("/availability/:id",verifyAdmin, updateRoomAvailability)
+//DELETE
+router.delete("/:id/:hotelid",verifyAdmin, deleteRoom);
+//GET
+
+router.get("/find/:id", getRoom);
+//GET ALL
+
+
+router.get("/", getRooms);
+
+export default router
